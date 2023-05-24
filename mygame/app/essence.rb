b68a772aef5f8865765703b8521ddc917723deae
Fernando Martínez <F-3r@users.noreply.github.com>
2023-05-24 01:28:32 -0300

    more boingy essence

--- mygame/app/essence.rb
+++ mygame/app/essence.rb
@@ -1,7 +1,7 @@
 def make_essence enemy
   speed = 5
   acceleration = 1
-  life_time = 400
+  life_time = 4000
   x = enemy.x
   y = enemy.y
   amount = enemy.essence_amount
@@ -37,14 +37,13 @@ def update_essence args
   args.state.essence_drops.each do |essence|
     essence.life_time -= 1
 
-    if essence.life_time < 600
-      direction = direction_between(essence, mouse)
-      distance = distance_between(essence, mouse)
-      essence.acceleration += 1 / distance if distance > 1
-      essence.speed = (essence.speed + essence.acceleration).clamp(0, 20)
-      essence[:x] += direction[:x] * essence.speed
-      essence[:y] += direction[:y] * essence.speed
-    end
+    direction = direction_between(essence, mouse)
+    distance = distance_between(essence, mouse)
+    essence.acceleration += 1 / distance if distance > 1
+    essence.speed = (essence.speed + essence.acceleration) if distance > 10
+    essence.speed = essence.speed.clamp(-60, 40)
+    essence[:x] += direction[:x] * essence.speed
+    essence[:y] += direction[:y] * essence.speed
 
     if essence.intersect_rect? base
       args.state.essence_held += essence.amount
@@ -52,8 +51,8 @@ def update_essence args
       essence.life_time = 0
     end
 
-    if essence.intersect_rect? mouse.merge(w: 20, h: 20)
-
+    if essence.intersect_rect? mouse.merge(w: 30, h: 30)
+      essence.speed *= -rand(5)
     end
   end
 end
